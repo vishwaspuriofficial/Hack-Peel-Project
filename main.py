60ae7cf469caf33a1a1105f5d0333291d1592db3
Vishwas Puri <vishwaspuriofficial@gmail.com>
2022-12-17 09:58:38 -0500

    Added Storage settings and buffer

--- main.py
+++ main.py
@@ -3,8 +3,24 @@ import time
 import datetime
 import pandas
 import os
+from humanize import naturalsize
 
+def doThis():    
+    pass
+
+#Storage Buffer Check
+size = 0
 storage = "Local Storage/"
+
+for path, dirs, files in os.walk(storage):
+    for f in files:
+        fp = os.path.join(path, f)
+        size += os.path.getsize(fp)
+
+print("Folder size: " + naturalsize(size))
+if size>1000000000: #more than 1 gb
+    doThis()
+
 startTime = datetime.datetime.now().strftime("%d-%m-%Y %H-%M-%S")
 os.mkdir(f"{storage}/{startTime}")
 os.mkdir(f"{storage}/{startTime}/Faces")
@@ -38,20 +54,12 @@ while True:
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     bodies = face_cascade.detectMultiScale(gray, 1.3, 5)
     counter = len(data)+1
-    # for (x,y,width,height) in faces:
-    #     cv2.rectangle(frame, (x,y),(x+width,y+height),(255,0,0),3)
-    #     cv2.drawMarker(frame,(x+width,y+height),(0,255,0))
     if len(faces) + len(bodies) > 0:
         if detection:
             timer_started = False
         else:
             detection = True
             timeIn = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")
-            # x,y,width,height = faces[-1]
-            # human = (int(x), int(y), int(width)+int(x), int(height)+int(y))
-            # face = ImageGrab.grab(bbox=human)
-            # img = cv2.cvtColor(np.array(face), cv2.COLOR_BGR2GRAY)
-            # cv2.imshow('test', np.array(img))
             cv2.imwrite(f"{storage}/{startTime}/Faces/face{counter}.jpg",frame)
             out = cv2.VideoWriter(
                 f"{storage}/{startTime}/Recordings/face{counter}.mkv", fourcc, 20, frame_size)
@@ -77,13 +85,10 @@ while True:
     if cv2.waitKey(1) == ord('q'):
         break
 print("Live Recording off!")
-print(data)
-
 for i in data:
     df = df.append({"Start":i[0], "End":i[1], "Face":i[2], "Recording":i[3]}, ignore_index = True)
 endTime = datetime.datetime.now().strftime("%d-%m-%Y %H-%M-%S")
 df.to_csv(f"{storage}/{startTime}/Security Data {startTime} to {endTime}.csv")
-
 out.release()
 cap.release()
 cv2.destroyAllWindows()
